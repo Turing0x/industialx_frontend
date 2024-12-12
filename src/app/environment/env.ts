75f441ced1939026fb90d6fe59d78db9cd455622
Turing0x <rauligf97@gmail.com>
2024-12-12 14:30:28 -0500

    chore: Update base URL for production environment
    - Change base URL from local development server to production API endpoint
    - Comment out local development URL for clarity
    
    This update ensures that the application points to the correct API for production use, facilitating a smoother deployment process.

--- src/app/environment/env.ts
+++ src/app/environment/env.ts
@@ -1,6 +1,6 @@
 export class Environments {
   static urlPattern: string =
     '^(https?|ftp)://([a-zA-Z0-9.-]+.[a-zA-Z]{2,6}|localhost)(:d{2,5})?(/[a-zA-Z0-9._%+-/?=&#]*)?$';
-  static baseUrl: string = 'http://localhost:8080/api';
-  // static baseUrl: string = 'https://industrialx.industrialxtore.com/api';
+  // static baseUrl: string = 'http://localhost:8080/api';
+  static baseUrl: string = 'https://industrialx.industrialxtore.com/api';
 }
